5047b9ff3f3da42344c8a3eb292324cdbafeb2ae
Ya Jin <qooq147852@gmail.com>
2022-04-06 02:12:27 +0800

    read the plaintext from file

--- AES-CTRmode.py
+++ AES-CTRmode.py
@@ -1,6 +1,7 @@
 import os
 import time
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
 
 start_time = time.time()
 
@@ -8,25 +9,28 @@ start_time = time.time()
 key = os.urandom(32)
 iv = os.urandom(16)
 
+print ("-- CTR --")
 
-message = b"a secret message"
 
+with open("context.txt", "rb") as file:
+    file_data = file.read()
 
+padder = padding.PKCS7(128).padder()
+padded_data = padder.update(file_data)
 cipher = Cipher(algorithms.AES(key), modes.CTR(iv))
 
 # Encryption
-for i in range(100000):
-    encryptor = cipher.encryptor()
-    ciphertext = encryptor.update(message) + encryptor.finalize()
+encryptor = cipher.encryptor()
+ciphertext = encryptor.update(padded_data) + encryptor.finalize()
     
 execute_time = (time.time() - start_time)
 print("--- %s seconds ---" % execute_time)
-print("--- {:.2f}bytes/s ---".format(800000/execute_time))
+print("--- {:.2f}bytes/s ---".format(324867676/execute_time))
 
 # Decryption
 decryptor = cipher.decryptor()
 plaintext = decryptor.update(ciphertext) + decryptor.finalize()
 
-print ("-- CTR --")
-print ("Ciphertext is:", ciphertext)
-print ("Plaintext is:", plaintext)
+
+#print ("Ciphertext is:", ciphertext)
+#print ("Plaintext is:", plaintext)
